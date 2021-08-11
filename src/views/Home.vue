360e006a5d705d284e6d433fd7fb01cd6c6a0e86
Loic <loicf69@yahoo.fr>
2021-08-12 01:36:24 +0200

    Mobile & tablet design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,55 @@
 <template>
-  <div>
-    Start project
+  <div class="header">
+    <img class="logo" src="../assets/logo.svg" />
+
+    <div class="hero">
+      <img class="tablet-hero" src="../assets/tablet/image-hero.png" />
+      <img class="hero-desktop hero-left" />
+      <img class="hero-desktop hero-right" />
+      <div class="hero-content">
+        <h1>
+          Group Chat<br />
+          for Everyone
+        </h1>
+        <p>
+          Meet makes it easy to connect with others face-to-face virtually and
+          collaborate across any device.
+        </p>
+        <div class="hero-buttons">
+          <button class="blue">Download <span>v1.3</span></button>
+          <button class="purple">What is it?</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="dot">
+    <div>
+      <span>01</span>
+    </div>
+  </div>
+  <div class="photo-grid">
+    <img src="../assets/desktop/image-woman-in-videocall.jpg">
+    <img src="../assets/desktop/image-women-videochatting.jpg">
+    <img src="../assets/desktop/image-men-in-meeting.jpg">
+    <img src="../assets/desktop/image-man-texting.jpg">
+  </div>
+  <div class="content">
+    <h3>Built for modern use</h3>
+    <h2>Smarter meetings, all in one place</h2>
+    <p>Send messages, share files, show your screen, and record your meetings — all in one workspace. Control who can join with invite-only team access, data encryption, and data export.</p>
+  </div>
+  <div class="footer">
+    <div class="bg-img"/>
+    <div class="dot two">
+    <div>
+      <span>02</span>
+    </div>
+  </div>
+   <div class="footer-content">
+      <h2>Experience more together</h2>
+      <p>Stay connected with reliable HD meetings and unlimited one-on-one and group video sessions.</p>
+      <button class="purple">Download <span>v1.3</span></button>
+    </div>
   </div>
 </template>
 
